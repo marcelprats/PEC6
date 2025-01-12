afc4204c6350b59f8442e6eba2f631e8e226b1e4
[Marcel Prats Ustrell] <marcelprats@uoc.edu>
2025-01-12 22:32:52 +0100

    Implementados los métodos en article-service y pruebas básicas

--- PAC6_Ej_Prac/ecommerce/src/app/services/article.service.spec.ts
+++ PAC6_Ej_Prac/ecommerce/src/app/services/article.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ArticleService } from './article.service';
 
 describe('ArticleService', () => {
@@ -12,5 +11,27 @@ describe('ArticleService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return all articles', (done: DoneFn) => {
+    service.getArticles().subscribe(articles => {
+      expect(articles.length).toBe(3);
+      done();
+    });
+  });
+
+  it('should change article quantity', (done: DoneFn) => {
+    service.changeQuantity(1, 5).subscribe(article => {
+      expect(article.quantity).toBe(15); // Inicialmente era 10
+      done();
+    });
+  });
+
+  it('should add a new article', (done: DoneFn) => {
+    const newArticle = { id: 4, name: 'Artículo 4', quantity: 20 };
+    service.create(newArticle).subscribe(article => {
+      expect(article.name).toBe('Artículo 4');
+      done();
+    });
   });
 });
--- PAC6_Ej_Prac/ecommerce/src/app/services/article.service.ts
+++ PAC6_Ej_Prac/ecommerce/src/app/services/article.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+export interface Article {
+  id: number;
+  name: string;
+  quantity: number;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ArticleService {
+  private articles: Article[] = [
+    { id: 1, name: 'Artículo 1', quantity: 10 },
+    { id: 2, name: 'Artículo 2', quantity: 5 },
+    { id: 3, name: 'Artículo 3', quantity: 15 },
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  // Obtener todos los artículos
+  getArticles(): Observable<Article[]> {
+    return of(this.articles);
+  }
+
+  // Cambiar la cantidad de un artículo
+  changeQuantity(articleID: number, changeInQuantity: number): Observable<Article> {
+    const article = this.articles.find(a => a.id === articleID);
+    if (article) {
+      article.quantity += changeInQuantity;
+      return of(article);
+    }
+    throw new Error('Artículo no encontrado');
+  }
+
+  // Crear un nuevo artículo
+  create(article: Article): Observable<Article> {
+    this.articles.push(article);
+    return of(article);
+  }
 }
